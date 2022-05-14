52557460d71da9768253a6d8136096cf8031965b
TamirMareli <tqmirmareli4949@gmail.com>
2022-05-14 11:49:37 +0300

    שינוי בהקצאה

--- Q4/main.c
+++ Q4/main.c
@@ -53,7 +53,7 @@ int main()
 		free(testReadStudents[i].grades);
 	}
 	free(testReadStudents);
-	
+
 
 
 	//CrtDumpMemoryLeaks();  //uncomment this block to check for heap memory allocation leaks.
@@ -64,7 +64,7 @@ int main()
 
 void countStudentsAndCourses(const char* fileName, int** coursesPerStudent, int* numberOfStudents)
 {
-	FILE* f=fopen("studentList.txt", "r");
+	FILE* f = fopen("studentList.txt", "r");
 	if (f == NULL) { exit(1); }
 	int numOfRow = 1;
 	int numOfCurse = 0;
@@ -77,11 +77,11 @@ void countStudentsAndCourses(const char* fileName, int** coursesPerStudent, int*
 	}
 	*numberOfStudents = numOfRow;
 	rewind(f);
-	int*numC = (int*)malloc(sizeof(int)*(*numberOfStudents));
+	int* numC = (int*)malloc(sizeof(int) * (*numberOfStudents));
 	if (numC == NULL) { printf("allocation failde"); exit(1); }
 	ch = fgetc(f);
 	for (int i = 0; i < numOfRow; i++) {
-		while (ch != '\n'&&!feof(f)) {
+		while (ch != '\n' && !feof(f)) {
 			if (ch == '|')
 				numOfCurse++;
 			ch = fgetc(f);
@@ -90,15 +90,15 @@ void countStudentsAndCourses(const char* fileName, int** coursesPerStudent, int*
 		numC[i] = (int*)malloc(sizeof(int));
 		if (numC == NULL) { printf("allocation failde"); exit(1); }
 		numC[i] = numOfCurse;
-		coursesPerStudent[i] = (numC+i);
-			numOfCurse = 0;
+		coursesPerStudent[i] = (numC + i);
+		numOfCurse = 0;
 	}
 	fclose(f);
 }
 
 int countPipes(const char* lineBuffer, int maxCount, char terminator)
 {
-	if (lineBuffer== NULL) { return -1; }
+	if (lineBuffer == NULL) { return -1; }
 	if (maxCount <= 0) { return 0; }
 	int count = 0;
 	for (int i = 0; i < maxCount; i++) {
@@ -116,11 +116,12 @@ char*** makeStudentArrayFromFile(const char* fileName, int** coursesPerStudent, 
 	int count = 0;
 	char ch;
 	if (f == NULL) { exit(1); }
-	char*** arrayS = (char***)malloc(sizeof(char**) *(*numberOfStudents) );
+	char*** arrayS = (char***)malloc(sizeof(char**) * (*numberOfStudents));
 	if (arrayS == NULL) { printf("alloction failde\n"); exit(1); }
 	for (int i = 0; i < *numberOfStudents; i++) {
-		arrayS[i] = (char**)malloc(sizeof(char*) * (1 + 2 * sizeof(*(coursesPerStudent[i]))));
-		if(arrayS[i]==NULL){ printf("alloction failde\n"); exit(1); }
+		int numberOfCourses = (*(*(coursesPerStudent)+i) * 2) + 1;
+		arrayS[i] = (char**)malloc(sizeof(char*) * numberOfCourses);
+		if (arrayS[i] == NULL) { printf("alloction failde\n"); exit(1); }
 		ch = fgetc(f);
 		for (int j = 0; j < (1 + 2 * (*coursesPerStudent[i])); j++) {
 			while (ch != '|' && ch != ',' && ch != '\n') {
@@ -136,19 +137,19 @@ char*** makeStudentArrayFromFile(const char* fileName, int** coursesPerStudent, 
 	ch = fgetc(f);
 	int index = 0;
 	for (int i = 0; i < *numberOfStudents; i++) {
-		for (int j = 0; j < (1+2*(*coursesPerStudent[i])); j++) {
-			while (ch != '|' && ch != ',' && ch != '\n'&&!feof(f)) {
+		for (int j = 0; j < (1 + 2 * (*coursesPerStudent[i])); j++) {
+			while (ch != '|' && ch != ',' && ch != '\n' && !feof(f)) {
 				arrayS[i][j][index] = ch;
 				index++;
 				ch = fgetc(f);
 			}
-			arrayS[i][j][index] ='\0';
+			arrayS[i][j][index] = '\0';
 			index = 0;
 			ch = fgetc(f);
 		}
 	}
 	fclose(f);
-	
+
 	return arrayS;
 }
 
@@ -160,11 +161,11 @@ void factorGivenCourse(char** const* students, const int* coursesPerStudent, int
 	int gread = 0;
 	int index = 0;
 	for (int i = 0; i < numberOfStudents; i++) {
-		for (int j = 0; j < 1 + 2 * coursesPerStudent[i];j++) {
-			if (strcmp(students[i][j], courseName)==0) {
-				while (*(students[i][j + 1]+index) != '\0') {
+		for (int j = 0; j < 1 + 2 * coursesPerStudent[i]; j++) {
+			if (strcmp(students[i][j], courseName) == 0) {
+				while (*(students[i][j + 1] + index) != '\0') {
 					gread *= 10;
-					gread += *(students[i][j + 1]+index) - 48;
+					gread += *(students[i][j + 1] + index) - 48;
 					index++;
 				}
 				gread += factor;
@@ -172,18 +173,18 @@ void factorGivenCourse(char** const* students, const int* coursesPerStudent, int
 					students[i][j + 1] = "100";
 				}
 				else
-				if (gread <=0) {
-					students[i][j + 1] = "0";
-				}
-				else {
-					students[i][j + 1][0] = 48 + (gread / 10);
-					students[i][j + 1][1] = 48 + (gread % 10);
-				}
+					if (gread <= 0) {
+						students[i][j + 1] = "0";
+					}
+					else {
+						students[i][j + 1][0] = 48 + (gread / 10);
+						students[i][j + 1][1] = 48 + (gread % 10);
+					}
 				gread = 0;
 				index = 0;
 			}
 		}
-  }
+	}
 }
 
 void printStudentArray(const char* const* const* students, const int* coursesPerStudent, int numberOfStudents)
@@ -208,20 +209,20 @@ void studentsToFile(char*** students, int* coursesPerStudent, int numberOfStuden
 		fprintf(f, "%s", "name: ");
 		fprintf(f, "%s\n", students[i][0]);
 		fprintf(f, "%s", "*********\n");
-		for (int j = 1; j < (1+2*coursesPerStudent[i]); j+=2) {
+		for (int j = 1; j < (1 + 2 * coursesPerStudent[i]); j += 2) {
 			fprintf(f, "%s", "course: ");
 			fprintf(f, "%s\n", students[i][j]);
 			fprintf(f, "%s", "grade: ");
-			fprintf(f, "%s\n\n", students[i][j+1]);
+			fprintf(f, "%s\n\n", students[i][j + 1]);
 		}
 	}
-/*
-	for (int i = 0; i < numberOfStudents; i++) {
-		//for(int j=0;j<1+2*(*coursesPerStudent);j++)
-		free(students[i][0]);
-	}
-	free(students);
-	*/
+	/*
+		for (int i = 0; i < numberOfStudents; i++) {
+			//for(int j=0;j<1+2*(*coursesPerStudent);j++)
+			free(students[i][0]);
+		}
+		free(students);
+		*/
 	fclose(f);
 }
 
@@ -231,9 +232,9 @@ void writeToBinFile(const char* fileName, Student* students, int numberOfStudent
 	if (f == NULL) { exit(1); }
 	fwrite(&numberOfStudents, sizeof(int), 1, f);
 	for (int i = 0; i < numberOfStudents; i++) {
-		fwrite(students[i].name, SIZE*sizeof(char), 1, f);
-		fwrite(&(students[i].numberOfCourses),sizeof(int), 1, f);
-		fwrite(students[i].grades,sizeof(StudentCourseGrade), students[i].numberOfCourses, f);
+		fwrite(students[i].name, SIZE * sizeof(char), 1, f);
+		fwrite(&(students[i].numberOfCourses), sizeof(int), 1, f);
+		fwrite(students[i].grades, sizeof(StudentCourseGrade), students[i].numberOfCourses, f);
 	}
 	fclose(f);
 }
@@ -252,9 +253,9 @@ Student* readFromBinFile(const char* fileName)
 		s[i].grades = (StudentCourseGrade*)malloc(sizeof(StudentCourseGrade) * s[i].numberOfCourses);
 		if (s[i].grades == NULL) { exit(1); }
 
-	    fread((s[i].grades), sizeof(StudentCourseGrade), s[i].numberOfCourses, f);
-	
-		}
+		fread((s[i].grades), sizeof(StudentCourseGrade), s[i].numberOfCourses, f);
+
+	}
 	fclose(f);
 	return s;
 
@@ -263,15 +264,15 @@ Student* readFromBinFile(const char* fileName)
 Student* transformStudentArray(char*** students, const int* coursesPerStudent, int numberOfStudents)
 {
 	int temp = 0;
-	Student* s = (Student*)malloc(sizeof(Student)*numberOfStudents);
+	Student* s = (Student*)malloc(sizeof(Student) * numberOfStudents);
 	if (s == NULL) { exit(1); }
 
 	for (int i = 0; i < numberOfStudents; i++) {
 		strcpy(s[i].name, students[i][0]);
 		s[i].numberOfCourses = coursesPerStudent[i];
-		s[i].grades = (StudentCourseGrade*)malloc(sizeof(StudentCourseGrade)*s[i].numberOfCourses);
+		s[i].grades = (StudentCourseGrade*)malloc(sizeof(StudentCourseGrade) * s[i].numberOfCourses);
 		if (s[i].grades == NULL) { exit(1); }
-		for (int j = 1; j < s[i].numberOfCourses; j+=2) {
+		for (int j = 1; j < s[i].numberOfCourses; j += 2) {
 			strcpy(s[i].grades->courseName, students[i][j]);
 			temp = atoi(students[i][j + 1]);
 			s[i].grades->grade = temp;
